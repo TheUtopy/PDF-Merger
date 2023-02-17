d866529d71b1590fca33f2f3b4cabb69dd551f68
Kévin C <kevin.chron@gmail.com>
2023-02-17 10:45:44 +0100

    Enter the name of the final file.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from PyPDF2 import PdfWriter as merger
 
 if __name__ == '__main__':
-        
+    # Demande le nom du fichier final
+    name_of_file = input("Enter the name of your merged file : ")
+
