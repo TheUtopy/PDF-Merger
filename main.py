e50d4ee3b9bd74948342e9b48865d7dd0743fbbf
Kévin C <kevin.chron@gmail.com>
2023-02-17 11:46:56 +0100

    Merge pdf on terminal command done.

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-from PyPDF2 import PdfWriter as merger
+from PyPDF2 import PdfWriter
+
+merger = PdfWriter()
 
 if __name__ == '__main__':
+
+
+    file_1 = input()
+    file_2 = input()
+    # "../test PDF Merger/01.pdf"
+
+    merger.append(file_1)
+    merger.append(file_2)
+
     # Demande le nom du fichier final
     name_of_file = input("Enter the name of your merged file : ")
 
+    # Demande le répertoire dans lequel enregistré le résultat final
+    output_path = input("Path of the output file : ")
+    if not output_path.endswith("/"):
+        output_path += "/"
+
+    # merge les fichiers
+    output = open(output_path + name_of_file + ".pdf", "wb")
+    merger.write(output)
+
+    merger.close()
+    output.close()
